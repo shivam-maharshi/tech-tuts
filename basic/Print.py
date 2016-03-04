f4bc05566504725e608cb568da59dc24a5d60204
shivam-maharshi <shivam.maharshi@gmail.com>
2016-03-04 02:34:06 -0500

    Added basic stuff like Printing and Structures in Python.

---
+++ basic/Print.py
@@ -0,0 +1,6 @@
+greetings = "Hello "
+name = "Shivam Maharshi"
+print (greetings + name[0:6])
+print("\n\nWe would like to refer to you as Mr. " + name[7:16])
+var = 1
+del var
---
+++ basic/Structures.py
@@ -0,0 +1,21 @@
+# List in Python can be hetrogenous type.
+
+aList = ['abc', 1234, "cde", 2.4, 546]
+bList = [1, 2]
+
+print(aList[0])
+print(aList[2:4])
+print (aList + bList)
+
+# Tuples size cannot be changed.
+
+aTuple = ( "abc", 12, 4.5, "bcde")
+bTuple = ("a", 98, 33, 'a')
+
+print(aTuple[0])
+print(bTuple*2)
+print(aTuple[1:3])
+print(aTuple + bTuple)
+
+# Dictionary is equivalent of Maps in python
+
