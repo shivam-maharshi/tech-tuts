5a40ebb3ecb5e8e05a88c07ecc43cc3c95ce6b83
shivam-maharshi <shivam.maharshi@gmail.com>
2016-03-04 03:10:35 -0500

    Added DataType conversion.

--- basic/DataType.py
+++ basic/DataType.py
@@ -6,8 +6,11 @@ bList = [1, 2]
 print(aList[0])
 print(aList[2:4])
 print (aList + bList)
+# Update list
+aList[2] = 1000
+print (aList)
 
-# Tuples size cannot be changed.
+# Tuples size cannot be changed - read only lists.
 
 aTuple = ( "abc", 12, 4.5, "bcde")
 bTuple = ("a", 98, 33, 'a')
@@ -18,4 +21,12 @@ print(aTuple[1:3])
 print(aTuple + bTuple)
 
 # Dictionary is equivalent of Maps in python
+
+dic = {}
+dic['key'] = "This is my value."
+dic[2] = "This is integer value."
+
+print (dic['key'])
+print (dic.keys())
+print (dic.values())
 
---
+++ basic/DataTypeConversion.py
@@ -0,0 +1,17 @@
+a = "15"
+print(int(a)) 
+print (int(a, 10))
+
+b = 6;
+print(hex(b))
+print(oct(b))
+
+print(eval(a))
+
+s = "124"
+print(list(s))
+
+print(set(s))
+
+d = (("key","value"),(2,3))
+print(dict(d))
