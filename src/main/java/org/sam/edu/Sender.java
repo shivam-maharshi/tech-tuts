c32df85f699b9d712870d32d70e3b57a21b18c0e
shivam-maharshi <shivam.maharshi@gmail.com>
2016-03-20 17:03:14 -0400

    Added a Sample Receiver and Sender class that publishes/consumes messages on queue.

---
+++ src/main/java/org/sam/edu/Sender.java
@@ -0,0 +1,28 @@
+package org.sam.edu;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+/**
+ * A sample Sender class that produces and puts messages onto queue.
+ * 
+ * @author shivam.maharshi
+ */
+public class Sender {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws java.io.IOException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		String message = "Hello World!";
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		channel.close();
+		connection.close();
+	}
+}
