b061a4b4b4d9b8c867229656508c1fe9eaf16c91
shivam-maharshi <shivam.maharshi@gmail.com>
2016-04-12 00:05:25 -0400

    Added Update call.

--- src/main/java/org/edu/Update.java
+++ src/main/java/org/edu/Update.java
@@ -7,13 +7,14 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 /**
- * Read operation.
+ * Update an entry.
  * 
  * @author shivam.maharshi
  */
-public class Read {
+public class Update {
 
 	public static void main(String[] args) {
 		try {
@@ -21,14 +22,17 @@ public class Read {
 			MongoDatabase db = client.getDatabase("local");
 			System.out.println("Connect to database successfully");
 			MongoCollection<Document> coll = db.getCollection("blog");
-			FindIterable<Document> findIt = coll.find();
+			FindIterable<Document> findIt = coll.find(Filters.eq("title", "MongoDB"));
 			MongoCursor<Document> cursor = findIt.iterator();
-			int i = 0;
-			while (cursor.hasNext()) {
-				System.out.println("Inserted document: " + i);
-				System.out.println("Cursor: " + cursor.next());
-				i++;
-			}
+			Document doc = null;
+			doc = cursor.next();
+			// Append field to doc.
+			doc.append("NewKey2", "NewValue2");
+			// Replaces the complete previous object with this new one.
+			// Only the _id is reused.
+			coll.replaceOne(Filters.eq("title", "MongoDB"), doc);
+			// Updates the document with this id to the new docs value.
+			coll.updateOne(Filters.eq("likes", "100"), new Document("title", "Cassandra"));
 			client.close();
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
